fdf4e34982d3732d81f3a3a13db1bb18a331fb42
weith <weith_007@sina.com>
2011-05-01 21:29:15 +0800

    Add a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #retune a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Weith Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
